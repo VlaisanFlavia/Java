4fcb74fffe8fea6261404fb7b11b9fefe21bf533
VlaisanFlavia <100787798+VlaisanFlavia@users.noreply.github.com>
2024-04-15 14:23:36 +0300

    Delete Lab_5 directory

--- Lab_5/src/main/java/org/lab5/InvalidCatalogException.java
+++
@@ -1,7 +0,0 @@
-package org.lab5;
-
-public class InvalidCatalogException extends Exception {
-    public InvalidCatalogException(Exception ex) {
-        super("Invalid catalog file.", ex);
-    }
-}
