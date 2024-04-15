da323f5a94c5a6c33982b8d2767558bb58a6e298
VlaisanFlavia <100787798+VlaisanFlavia@users.noreply.github.com>
2024-04-15 14:23:05 +0300

    Delete Lab_2 directory

--- Lab_2/src/LocationType.java
+++
@@ -1,7 +0,0 @@
-/**
- * This enumeration describes what types of locations we have
- */
-public enum LocationType {
-
-    CITY, AIRPORT, GAS_STATION, SCHOOL;
-}
--- Lab_2/src/RoadType.java
+++
@@ -1,7 +0,0 @@
-/**
- * This enumeration describes what types of roads we have
- */
-public enum RoadType {
-
-    HIGHWAY, EXPRESS, COUNTRY;
-}
